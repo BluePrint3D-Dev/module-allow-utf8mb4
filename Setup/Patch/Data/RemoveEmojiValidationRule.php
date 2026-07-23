8ab8000b331ed37b6317ef408fac8e8d29584dec
stuhares <stu@blueprint3d.co.uk>
2026-07-23 15:43:11 +0100

    chore: applied Magento EQP automated coding standards

--- Setup/Patch/Data/RemoveEmojiValidationRule.php
+++ Setup/Patch/Data/RemoveEmojiValidationRule.php
@@ -1,9 +1,9 @@
 <?php
 /**
  * Copyright (c) 2026 BluePrint3D Ltd. All rights reserved.
- * 
+ *
  * This software is provided free of charge for personal or commercial use.
- * Resale, redistribution, or sublicensing of this source code, modified or 
+ * Resale, redistribution, or sublicensing of this source code, modified or
  * unmodified, for direct financial gain is strictly prohibited.
  *
  * @author    BluePrint3D Ltd <support@blueprint3d.dev>
